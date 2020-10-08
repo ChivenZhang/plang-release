eb5a3b21ba6136e4ce49a1ec2d8b2d74b664d508
张喜文 <1759774248@qq.com>
2020-10-08 18:38:53 +0800

    'auto-update'

--- run_demo_src.bat
+++ run_demo_src.bat
@@ -4,4 +4,6 @@ cd %~dp0
 
 "bin/x64/plang.exe" -c Test -o library
 
+"bin/x86/plang32.exe" -c Test -o library
+
 pause
--- run_demo_test.bat
+++ run_demo_test.bat
@@ -6,4 +6,6 @@ cd %~dp0
 
 "bin/x64/plang.exe" -c test/Test
 
+"bin/x86/plang32.exe" -c test/Test
+
 pause
